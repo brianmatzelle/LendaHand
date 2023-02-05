83f7303846b410f30deb2feca6c784daccf2cf4d
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2023-02-04 21:49:17 -0500

    sample data added

--- lendahand-app/navigation/screens/lend-screens/ForYou.js
+++ lendahand-app/navigation/screens/lend-screens/ForYou.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { styles } from './Styles';
 
+const eventDict = [
+    { key: '1' },
+    { key: '2' },
+    { key: '3' },
+    { key: '4' }, 
+]
+
 export default function ForYou() {
     return (
         <ScrollView>
-            
+            { eventDict.map(obj => {
+                <View
+                    key={obj.key}
+                >
+
+                </View>
+            })}
         </ScrollView>
     );
 }
