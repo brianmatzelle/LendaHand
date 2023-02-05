2fca6fc991e9df43742f990b3b83cb45821aaa2c
sujkovic <adrian.sujkovic@gmail.com>
2023-02-05 08:03:12 -0500

    add create event controller

--- lendahand-app/lendahand-backend/controllers/eventController.js
+++ lendahand-app/lendahand-backend/controllers/eventController.js
@@ -24,9 +24,55 @@ exports.event_create_get = (req, res) => {
   res.send("NOT IMPLEMENTED: event create get");
 };
 
-exports.event_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: event create post");
-};
+exports.event_create_post = [
+  //  Convert attendants to array
+  (req, res, next) => {
+    if (!Array.isArray(req.body.attendants)) {
+      req.body.attendants =
+        typeof req.body.attendants === "undefined" ? [] : [req.body.genre];
+    }
+    next();
+  },
+  //  Validate and sanitize data
+  body("name", "Event name can't be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("t_event").escape(),
+  body("date", "Invalid Date").escape(),
+  body("x_loc").escape(),
+  body("y_loc").escape(),
+  body("description").escape(),
+  body("host").escape(),
+  body("attendants.*").escape(),
+  //  process request
+  (req, res, next) => {
+    // errors
+    const errors = validationResult(req);
+    //  event object
+    const event = new Event({
+      name: req.body.name,
+      t_event: req.body.t_event,
+      date: req.body.date,
+      x_loc: req.body.x_loc,
+      y_loc: req.body.y_loc,
+      description: req.body.description,
+      host: req.body.host,
+      attendants: req.body.attendants,
+    });
+    //  error stuff
+    if (!errors.isEmpty()) {
+      res.send({ event: req.body, errors: errors.array() });
+    }
+    //  data gucci
+    event.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      //res.redirect(event.url);
+    });
+  },
+];
 
 exports.event_delete_get = (req, res) => {
   res.send("NOT IMPLEMENTED: event delete get");
